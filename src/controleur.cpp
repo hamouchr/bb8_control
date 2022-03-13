ce36e666574ed18f08531374fc1296072ebd6bf8
hamouchr <74318193+hamouchr@users.noreply.github.com>
2022-03-13 18:55:44 +0100

    Update controleur.cpp
    Version 2021-2022
    Contrôleur sans publisher

--- src/controleur.cpp
+++ src/controleur.cpp
@@ -1,5 +1,4 @@
 // LES FICHIERS EN-TETES
-#include <geometry_msgs/Twist.h>
 #include <math.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
@@ -7,7 +6,6 @@
 // LES NAMESPACES
 using namespace ros;
 using namespace std_msgs;
-using namespace geometry_msgs;
 
 // LES CONSTANTES
 #define MODE_CIRCULAIRE 1
@@ -17,11 +15,9 @@ using namespace geometry_msgs;
 #define FREQ_CONTROL 10 // hz
 
 // LES VARIABLES GLOBALES
-Twist cmd;
-Publisher cmd_pub;
 int mode;
 
-// LES FONTIONS ET LES CALLBACKS
+// LES FONCTIONS ET LES CALLBACKS
 void avancer(float lin_vel) {}
 
 void reculer(float lin_vel) {}
@@ -42,15 +38,18 @@ int main(int argc, char **argv) {
   init(argc, argv, "controleur");
   NodeHandle nh;
 
-  cmd_pub = nh.advertise<Twist>("/cmd_vel", 100);
-
+  
+  
+  
+  
   Rate loop_rate(FREQ_CONTROL);
-
   while (ok()) {
 
-    mouvement_rectangulaire();
 
-    //
+
+
+
+
     spinOnce();
     loop_rate.sleep();
   }
